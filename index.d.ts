cf679a63f969fb16c7400fb85373b56f2c893cb3
Miguel Bca <miguel.cahuinou@gmail.com>
2025-01-18 00:17:10 -0500

    seconds commit add types

--- index.d.ts
+++ index.d.ts
@@ -1,99 +1,21 @@
-export interface SearchTypes {
-  ANY: "CAA%3D";
-  CHANNEL: "EgIQAg%3D%3D";
-  LIVE: "EgJAAQ%3D%3D";
-  MOVIE: "EgIQBA%3D%3D";
-  PLAYLIST: "EgIQAw%3D%3D";
-  VIDEO: "EgIQAQ%3D%3D";
-}
+declare module 'yt-api-search' {
+  export const constants: any;
+  export const SearchYouTube: any;
+  export const SearchTypes: { VIDEO: string; CHANNEL: string; PLAYLIST: string };
+  export const YouTubeURL: URL;
 
-export interface Thumbnail {
-  url: string;
-  width: number;
-  height: number;
-}
-
-export interface GeneralData {
-  id: string;
-  link: string;
-  thumbnail: string;
-  title: string;
-}
-
-export interface ShareableGeneralData extends GeneralData {
-  shareLink: string;
-}
-
-export interface VideoPreview extends ShareableGeneralData {
-  duration: number;
-  duration_raw: string;
-  views: number;
-}
-
-export interface ChannelPreview {
-  name: string;
-  link: string;
-  verified: boolean;
-}
-
-export interface Video extends ShareableGeneralData {
-  description: string;
-  duration: number;
-  duration_raw: string;
-  uploaded: string;
-  views: number;
-  channel: ChannelPreview;
-}
-
-export interface Playlist extends GeneralData {
-  preview: VideoPreview[];
-  duration: number;
-  duration_raw: string;
-}
-
-export interface Stream extends ShareableGeneralData {
-  watching: number;
-  channel: ChannelPreview;
-}
-
-export interface Channel {
-  channelId: string;
-  description: string;
-  link: string;
-  thumbnails: Thumbnail[];
-  subscribed: boolean;
-  uploadedVideos: number;
-  verified: boolean;
-}
-
-export interface Results {
-  channels: Channel[];
-  playlists: Playlist[];
-  streams: Stream[];
-  videos: Video[];
-}
-
-export interface SearchOptions {
-  searchType?: keyof SearchTypes;
-  language?: string;
-}
-
-declare class Scraper {
-  private _lang: string;
-
-  public constructor(language?: string);
-
-  private _extractData(
-    json: Record<string, unknown>
-  ): Record<string, unknown>[];
-  private _fetch(
-    search_query: string,
-    searchType?: keyof SearchTypes,
-    requestedLang?: string
-  ): Promise<string>;
-  private _getSearchData(webPage: string): Record<string, unknown>;
-  private _parseData(data: Record<string, unknown>[]): Results;
-
-  public search(query: string, options?: SearchOptions): Promise<Results>;
-  public setLang(language?: string): void;
+  export function compress(key: any): string;
+  export function getChannelLink(cRender: any): string;
+  export function getPlaylistVideoData(renderer: any): any;
+  export function getPlaylistGeneralData(pRender: any): any;
+  export function getChannel(vRender: any): any;
+  export function parseDuration(vRender: any): number;
+  export function getDuration(vRender: any): string;
+  export function textDuration(vRender: any): string;
+  export function getViews(vRender: any): number;
+  export function parseViews(vRender: any): string;
+  export function textViews(vRender: any): string;
+  export function shortTextViews(vRender: any): string;
+  export function getUploadDate(vRender: any): string;
+  export function getDescription(vRender: any): string;
 }
